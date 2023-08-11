02c711eda1d567cac6997cdc23874d61adf2420c
August Aldred <hachigatsu14@outlook.com>
2023-08-11 08:44:55 +0100

    add homepage image

--- src/components/WelcomeItem.vue
+++ src/components/WelcomeItem.vue
@@ -2,10 +2,43 @@
   <header class="header">
     <h1>Welcome to Pansy Tours!</h1>
   </header>
+  <img class="home-img" :src="pansy" alt="a purple pansy flower" />
 </template>
 
 <style scoped>
 .header {
   text-align: center;
 }
+
+.home-img {
+  margin: 0 auto;
+  display: block;
+  max-width: 100%;
+  border-radius: 70px 150px;
+  border: 6px black double;
+}
+
+@media (prefers-color-scheme: dark) {
+  .home-img {
+    border: 6px ghostwhite double;
+  }
+}
+
+@media (min-width: 768px) {
+  .home-img {
+    max-width: 70%;
+  }
+}
 </style>
+
+<script>
+import pansy from "../assets/pansy.avif";
+
+export default {
+  data: function () {
+    return {
+      pansy: pansy,
+    };
+  },
+};
+</script>
