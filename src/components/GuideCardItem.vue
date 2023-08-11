99aea5dbcef4db6d8a0876db3a09bf9af70852f1
August Aldred <hachigatsu14@outlook.com>
2023-08-11 16:18:20 +0100

    Create separate component for guide cards

---
+++ src/components/GuideCardItem.vue
@@ -0,0 +1,24 @@
+<style></style>
+
+<template>
+  <img class="card-img" :src="img" alt="" />
+  <h3 class="card-header">{{ guideName }}</h3>
+  <p>Pronouns: {{ pronouns }}</p>
+  <p>Speaks: {{ languages }}</p>
+  <p @click="viewProfile(id)">View profile</p>
+</template>
+
+<script>
+export default {
+  name: "GuideCardITem",
+  methods: {
+    viewProfile(guide_id) {
+      this.$router.push({
+        name: "single guide",
+        params: { guide_id: guide_id },
+      });
+    },
+  },
+  props: ["id", "img", "guideName", "pronouns", "languages"],
+};
+</script>
