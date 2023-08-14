22e576b1902826d58d0746b5fd2a1fd0acce13af
August Aldred <hachigatsu14@outlook.com>
2023-08-14 16:58:13 +0100

    Remove isLoading references

--- src/store/bookings.js
+++ src/store/bookings.js
@@ -4,18 +4,14 @@ import axios from "axios";
 export const useBookingsStore = defineStore("bookings", {
   state: () => ({
     bookings: [],
-    isLoading: false,
   }),
   actions: {
     getBookings() {
-      this.isLoading = true;
       axios
         .get("https://be-vue-booking.onrender.com/api/bookings")
         .then(({ data: { bookings } }) => {
           this.bookings = bookings;
         });
-
-      this.isLoading = false;
     },
   },
 });
--- src/store/tours.js
+++ src/store/tours.js
@@ -4,18 +4,14 @@ import axios from "axios";
 export const useToursStore = defineStore("tours", {
   state: () => ({
     tours: [],
-    isLoading: false,
   }),
   actions: {
     getTours() {
-      this.isLoading = true;
       axios
         .get("https://be-vue-booking.onrender.com/api/tours")
         .then(({ data: { tours } }) => {
           this.tours = tours;
         });
-
-      this.isLoading = false;
     },
   },
 });
